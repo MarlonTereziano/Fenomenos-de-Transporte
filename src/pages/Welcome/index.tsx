38227f480d3de34a25ddc5db51ffa2f304a0d12f
Marlon Tereziano <80834228+MarlonTereziano@users.noreply.github.com>
2022-07-04 16:02:06 -0300

    update: box styles

--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -1,12 +1,15 @@
-import {Container} from './styles';
+import { Container, SideBar, Content } from './styles';
 import React from 'react';
 
 export const Welcome: React.FC = () => {
     return (
         <Container>
-            <h1>Welcome</h1>
+            <SideBar/>
+            <Content>
+                <div className="ft">Fenômenos de Transporte</div>
+                <div className="maps"></div>
+            </Content>
         </Container>
-
     );
 };
 
--- src/pages/Welcome/styles.ts
+++ src/pages/Welcome/styles.ts
@@ -1,8 +1,44 @@
 import styled from 'styled-components';
 export const Container = styled.div`
-    width: 100%;
-    height: 100%;
-    background-color: #blue;
+    width: 100vw;
+    height: 100vh;
+    display:flex;
+    align-items: center;
+
+    .maps{
+        align-items: center;
+        justify-content: center;
+        display: flex;
+        border-radius: 15px;
+        display: flex;
+        width: 70%;
+        height: 70%;
+        box-shadow: 3px 3px 15px rgba(0,0,0,0.5);
+        font-size: 5rem;
+    }
+
+    .ft{
+        position: absolute;
+        top: 0;
+        margin-top: 1%;
+        font-size: 3rem;
+    }
+
 `
 
+export const SideBar = styled.div`
+    left: 0;
+    width: 20%;
+    height: 100%;
+    background-color: grey;
+    border-radius: 10px;
 
+`
+
+export const Content = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
+`
